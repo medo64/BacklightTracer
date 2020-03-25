d4332a3245f707d2dc2c6a728b75837b4384651e
Josip Medved <jmedved@jmedved.com>
2020-03-24 22:42:07 -0700

    Adding backlight control via files

--- src/backlight-tracer.sh
+++ src/backlight-tracer.sh
@@ -3,8 +3,9 @@
 if [ -t 1 ]; then
     ESCAPE_RESET="\E[0m"
     ESCAPE_ERROR="\E[31;1m"
+    ESCAPE_WARNING="\E[33;1m"
     ESCAPE_CHANGE="\E[37m"
-    ESCAPE_RESTORE="\E[33;1m"
+    ESCAPE_RESTORE="\E[36;1m"
 fi
 
 
@@ -25,6 +26,9 @@ trap "echo -ne '${ESCAPE_RESET}' ; rm $PID_FILE 2> /dev/null; exit 0" EXIT INT K
 FILE_DATA_ADAPTER="/var/cache/.backlight-tracer.adapter.dat"
 FILE_DATA_BATTERY="/var/cache/.backlight-tracer.battery.dat"
 
+FILE_INPUT_INCREASE="/run/.backlight-tracer.inc.signal"
+FILE_INPUT_DECREASE="/run/.backlight-tracer.dec.signal"
+
 if [ "$EUID" -ne 0 ]; then
     echo -e "${ESCAPE_ERROR}$SCRIPT_NAME: must run as root (try sudo)!${ESCAPE_RESET}" >&2
     exit 254
@@ -34,6 +38,17 @@ FILE_DEVICE_BACKLIGHT_BRIGHTNESS="/sys/class/backlight/intel_backlight/brightnes
 if [[ ! -e "$FILE_DEVICE_BACKLIGHT_BRIGHTNESS" ]]; then
     echo -e "${ESCAPE_ERROR}$SCRIPT_NAME: cannot find backlight brightness in '$FILE_DEVICE_BACKLIGHT_BRIGHTNESS'!${ESCAPE_RESET}" >&2
     exit 252
+fi
+
+FILE_DEVICE_BACKLIGHT_BRIGHTNESS_MAX="/sys/class/backlight/intel_backlight/max_brightness"
+if [[ ! -e "$FILE_DEVICE_BACKLIGHT_BRIGHTNESS_MAX" ]]; then
+    echo -e "${ESCAPE_WARNING}$SCRIPT_NAME: cannot find maximum backlight brightness in '$FILE_DEVICE_BACKLIGHT_BRIGHTNESS_MAX'${ESCAPE_RESET}" >&2
+else
+    MAX_BRIGHTNESS=`cat $FILE_DEVICE_BACKLIGHT_BRIGHTNESS_MAX`
+    STEP_BRIGHTNESS=$(( $MAX_BRIGHTNESS / 20 ))
+    if [[ ! "$STEP_BRIGHTNESS" -gt 0 ]]; then
+        echo -e "${ESCAPE_WARNING}$SCRIPT_NAME: cannot determine backlight brightness step in '$FILE_DEVICE_BACKLIGHT_BRIGHTNESS_MAX'${ESCAPE_RESET}" >&2
+    fi
 fi
 
 if [[ -e /sys/class/power_supply/AC/online ]]; then
@@ -85,5 +100,27 @@ while(true); do
     fi
 
     sleep 0.5
+
+    if [[ "$STEP_BRIGHTNESS" -gt 0 ]]; then
+        if [[ -e "$FILE_INPUT_DECREASE" ]]; then
+            rm "$FILE_INPUT_DECREASE"
+            NEW_BRIGHTNESS=$(( $CURR_BRIGHTNESS - $STEP_BRIGHTNESS ))
+            if [[ "$NEW_BRIGHTNESS" -lt 1 ]]; then NEW_BRIGHTNESS=1; fi
+            if [[ "$NEW_BRIGHTNESS" -ne "$CURR_BRIGHTNESS" ]]; then
+                echo "$NEW_BRIGHTNESS" > "$FILE_DEVICE_BACKLIGHT_BRIGHTNESS"
+            fi
+        elif [[ -e "$FILE_INPUT_INCREASE" ]]; then
+            rm "$FILE_INPUT_INCREASE"
+            if [[ "$CURR_BRIGHTNESS" -lt "$STEP_BRIGHTNESS" ]]; then
+                NEW_BRIGHTNESS=$STEP_BRIGHTNESS
+            else
+                NEW_BRIGHTNESS=$(( $CURR_BRIGHTNESS + $STEP_BRIGHTNESS ))
+                if [[ "$NEW_BRIGHTNESS" -gt "$MAX_BRIGHTNESS" ]]; then NEW_BRIGHTNESS=$MAX_BRIGHTNESS; fi
+            fi
+            if [[ "$NEW_BRIGHTNESS" -ne "$CURR_BRIGHTNESS" ]]; then
+                echo "$NEW_BRIGHTNESS" > "$FILE_DEVICE_BACKLIGHT_BRIGHTNESS"
+            fi
+        fi
+    fi
 done
 
